3cd72566e6b03bbe98264a4abf56cbc5c2807b34
alex <147138151+codralexandra@users.noreply.github.com>
2024-04-01 11:39:35 +0300

    added view-model for gamewindow, model for the board tiles and a service for the game logic
    to implement the ui for the game board (with images)

---
+++ WpfApp1Tema2 - Checkers (New)/WpfApp1Tema2 - Checkers (New)/Service/PieceColors.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1Tema2___Checkers__New_.Service
+{
+    internal enum PieceColors
+    {
+        None = 0,
+        White = 1,
+        Red = 2,
+    }
+}
---
+++ WpfApp1Tema2 - Checkers (New)/WpfApp1Tema2 - Checkers (New)/View-Models/GameWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1Tema2___Checkers__New_.Models;
+using WpfApp1Tema2___Checkers__New_.Service;
+
+namespace WpfApp1Tema2___Checkers__New_.View_Models
+{
+    internal class GameWindowViewModel
+    {
+        private ObservableCollection<ObservableCollection<BoardSquareModel>> gameBoard { get; set; }
+        private GameBusinessLogic gameLogic { get; set; }
+
+        public GameWindowViewModel()
+        {
+            gameBoard = gameLogic.initializeGameBoard();
+        }
+    }
+}
